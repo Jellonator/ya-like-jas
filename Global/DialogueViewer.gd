b716d88abbe8e2f85fd9854256b5f24cda2edf5a
Jocelyn Beedie <jellonator00@gmail.com>
2020-04-28 11:12:05 -0400

    Dialogue should now obey bbcode

--- Global/DialogueViewer.gd
+++ Global/DialogueViewer.gd
@@ -1,45 +1,48 @@
 extends CanvasLayer
 
-const TEXT := "According to all the known laws of aviation, there is no way that" +\
+const SAMPLETEXT := "According to all the known laws of aviation, there is no way that" +\
 	" the bumblebee should be able to fly.\n" +\
-	"The bee, of course, flies anyways, because bees don't care what humans think" +\
-	" is impossible."
+	"The [color=#FFCC00]bee[/color], of course, flies anyways, because bees don't care what humans think" +\
+	" is impossible." + "\n  teehee"
 
 const MAXWIDTH := 992.0
 
-var dialogue_stack := []
+var current_scroll := 0
+var num_lines := 0
+var num_visible_at_line := []
 
 onready var node_label: RichTextLabel = $Display/Label
-
-func read_from_dialogue_stack():
-	node_label.visible_characters = 0
-	node_label.clear()
-	for i in range(min(3, dialogue_stack.size())):
-		if i > 0:
-			node_label.newline()
-		node_label.add_text(dialogue_stack.pop_back())
+onready var node_disp: Control = $Display
 
 func _ready():
-	node_label.rect_clip_content = false
-	show_dialogue(TEXT)
+	show_dialogue(SAMPLETEXT)
 
 func split_text(text: String) -> PoolStringArray:
 	var ret := PoolStringArray()
 	var font := node_label.get_font("normal_font") as Font
 	var current_line := ""
 	var current_size := 0.0
+	var is_in_bracket := false
 	for line in text.split('\n'):
 		for word in line.split(' ', true):
-			if word == "":
-				word = " "
+			var visible_word := ""
+			for c in word:
+				if is_in_bracket:
+					if c == ']':
+						is_in_bracket = false
+				else:
+					if c == '[':
+						is_in_bracket = true
+					else:
+						visible_word += c
 			if current_size == 0.0:
-				current_size = font.get_string_size(word).x
+				current_size = font.get_string_size(visible_word).x
 				current_line = word
 			else:
-				var wordsize := font.get_string_size(" " + word).x
+				var wordsize := font.get_string_size(" " + visible_word).x
 				if current_size + wordsize > MAXWIDTH:
 					ret.append(current_line)
-					current_size = font.get_string_size(word).x
+					current_size = font.get_string_size(visible_word).x
 					current_line = word
 				else:
 					current_size += wordsize
@@ -56,19 +59,45 @@ func split_text(text: String) -> PoolStringArray:
 func show_dialogue(text: String):
 	node_label.clear()
 	var arr := split_text(text)
+	while arr.size() % 3 != 0:
+		arr.append("a")
+	node_label.clear()
+	var s := ""
+	var n := 0
+	var is_in_bracket := false
 	for i in range(arr.size()):
-		dialogue_stack.push_back(arr[arr.size() - i - 1])
-	read_from_dialogue_stack()
+		num_visible_at_line.append(n)
+		if i > 0:
+			s += '\n'
+		s += arr[i]
+		for c in arr[i]:
+			if is_in_bracket:
+				if c == ']':
+					is_in_bracket = false
+			else:
+				if c == '[':
+					is_in_bracket = true
+				else:
+					n += 1
+	node_label.parse_bbcode(s)
+	num_lines = arr.size()
+	current_scroll = 0
 
 func show_options(text: String, options: Array):
 	pass
+
+func finish_dialogue():
+	node_disp.hide()
 
 func _physics_process(delta):
 	var vis := node_label.visible_characters
 	if vis < node_label.get_total_character_count():
 		node_label.visible_characters += 1
 	if Input.is_action_just_pressed("on_click"):
-		if dialogue_stack.size() == 0:
-			print("FINISHED")
+		current_scroll += 3
+		if current_scroll >= num_lines:
+			finish_dialogue()
 		else:
-			read_from_dialogue_stack()
+			node_label.scroll_to_line(current_scroll)
+			if num_visible_at_line.size() > current_scroll:
+				node_label.visible_characters = num_visible_at_line[current_scroll]
